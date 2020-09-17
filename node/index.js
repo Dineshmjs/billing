23bb454f4f69db4a1a93347d6544c6e10cf0aeb6
Spe <dineshmj82@gmail.com>
2020-09-17 10:51:37 +0530

    Revert "init my lap"
    This reverts commit e95b061ff471537cad4f6bbb235d5bc47c7ee403.

---
+++ node/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(morgan("dev")); 
+app.use(express.json())
+
+const product = require('./router/Product')
+
+app.use("/product",product)
+
+app.get("/",(req,res)=>{
+    res.json("Root")
+})
+
+mongoose.connect("mongodb://localhost:27017/bill",{ useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+    if(!err){
+        console.log("Mongodb Connectced")
+    }
+    if(err){
+        console.log(err)
+    }
+})
+
+app.listen(2000,()=>{
+    console.log("Server run port 2000")
+})
+
