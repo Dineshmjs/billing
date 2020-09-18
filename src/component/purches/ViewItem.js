f8d8f291aff324df29911cb01d8e19b9400b45f4
Spe <dineshmj82@gmail.com>
2020-09-18 19:17:32 +0530

    purches tempitems

--- src/component/purches/ViewItem.js
+++ src/component/purches/ViewItem.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { http } from '../../axios'
 
 function ViewItem() {
+    const [viewdata,setviewData] = useState([])
     useEffect(() => {
         http.get("purches/tempItems")
             .then(res=>{
                 console.log(res.data)
+                setviewData(res.data)
             })
             .catch(err=>{
                 console.log(err)
@@ -13,7 +15,9 @@ function ViewItem() {
     }, [])
     return (
         <div>
-            
+            {
+                viewdata.map(data=><p>{data.product}</p>)
+            }
         </div>
     )
 }
