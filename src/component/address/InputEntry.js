3a8a81ea8937dce449ce97dac50e1cf89716f1d6
Spe <dineshmj82@gmail.com>
2020-09-22 19:22:50 +0530

    update and delete address

--- src/component/address/InputEntry.js
+++ src/component/address/InputEntry.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as yup from 'yup'
 import './index.css'
-import {http} from '../../axios'
+import { http } from '../../axios'
 
-function InputEntry() {
+function InputEntry({ Reload, updateValues, Update, update, insert }) {
+
+
     const initialValues = {
         name: "",
         gstin: "",
@@ -17,20 +19,25 @@ function InputEntry() {
         pincode: ""
     }
 
+
+
     const submit = (values, submitProps) => {
         console.log("form values", values)
-        http.post("address",values)
-            .then(res=>{
+        http.post("address", values)
+            .then(res => {
                 console.log(res.data)
+                Reload(res.data._id)
+
             })
-            .catch(err=>{
+            .catch(err => {
                 console.log(err)
             })
         submitProps.resetForm()
     }
 
+    
     const validationSchema = yup.object({
-        name: yup.string().required("Please Enter Name"),        
+        name: yup.string().required("Please Enter Name"),
         gstin: yup.string().required("Please Enter GSTIN"),
         contact: yup.number().required("Please Enter Mobile No"),
         email: yup.string().email("Invalid Email").required("Please Enter Email-Id "),
@@ -43,59 +50,75 @@ function InputEntry() {
     return (
         <div className="container mt-3">
             <Formik
-                initialValues={initialValues}
+                initialValues={updateValues || initialValues}
                 onSubmit={submit}
                 validationSchema={validationSchema}
+                enableReinitialize
             >
-                {/* {
+                {
+                    (formikProps) =>{
+                        console.log("formikProps",formikProps)
+                    
+                    
+                    return (
+                        <Form className="form">
+                            <div className="form-group">
+                                <Field name="name" type="text" placeholder="Enter Name Of The Company" className="form-control" />
+                                <ErrorMessage name="name" />
+                            </div>
+                            <div className="form-group">
+                                <Field name="gstin" type="text" placeholder="Company GSTIN NO" className="form-control" />
+                                <ErrorMessage name="gstin" />
+                            </div>
+                            <div className="contactDetails">
+                                <div className="form-group">
+                                    <Field name="contact" type="number" placeholder="Enter Mobile No" className="form-control" />
+                                    <ErrorMessage name="contact" />
+                                </div>
+                                <div className="form-group">
+                                    <Field name="email" type="text" placeholder="Enter Email Id" className="form-control" />
+                                    <ErrorMessage name="email" />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <Field name="address" type="text" placeholder="Eg : Door No, Street, Area Name" className="form-control" />
+                                <ErrorMessage name="address" />
+                            </div>
+                            <div className="addressInput">
+                                <div className="form-group">
+                                    <Field name="city" type="text" placeholder="Enter City" className="form-control" />
+                                    <ErrorMessage name="city" />
+                                </div>
+                                <div className="form-group">
+                                    <Field name="state" type="text" placeholder="Enter State" className="form-control" />
+                                    <ErrorMessage name="state" />
+                                </div>
+                                <div className="form-group">
+                                    <Field name="country" type="text" placeholder="Enter Country" className="form-control" />
+                                    <ErrorMessage name="country" />
+                                </div>
+                                <div className="form-group">
+                                    <Field name="pincode" type="number" placeholder="Enter Pincode" className="form-control" />
+                                    <ErrorMessage name="pincode" />
+                                </div>
+                            </div>
+                            <div className="form-group w3-center">
+                                {/* <button className="w3-button w3-indigo" type="reset" onClick={()=>formikProps.resetForm()}>Reset</button> */}
+                                {
+                                    insert && <button className="w3-button w3-purple" type="submit">Submit</button>
+                                }
+                                {
 
-                   (formiklog)=> console.log(formiklog)
-                } */}
-                <Form className="form">
-                    <div className="form-group">
-                        <Field name="name" type="text" placeholder="Enter Name Of The Company" className="form-control" />
-                        <ErrorMessage name="name" />
-                    </div>
-                    <div className="form-group">
-                        <Field name="gstin" type="text" placeholder="Company GSTIN NO" className="form-control" />
-                        <ErrorMessage name="gstin" />
-                    </div>
-                    <div className="contactDetails">
-                        <div className="form-group">
-                            <Field name="contact" type="number" placeholder="Enter Mobile No" className="form-control" />
-                            <ErrorMessage name="contact" />
-                        </div>
-                        <div className="form-group">
-                            <Field name="email" type="text" placeholder="Enter Email Id" className="form-control" />
-                            <ErrorMessage name="email" />
-                        </div>
-                    </div>                   
-                    <div className="form-group">
-                        <Field name="address" type="text" placeholder="Eg : Door No, Street, Area Name" className="form-control" />
-                        <ErrorMessage name="address" />
-                    </div>
-                    <div className="addressInput">
-                        <div className="form-group">
-                            <Field name="city" type="text"  placeholder="Enter City"  className="form-control" />
-                            <ErrorMessage  name="city" />
-                        </div>
-                        <div className="form-group">
-                            <Field name="state" type="text" placeholder="Enter State" className="form-control" />
-                            <ErrorMessage  name="state" />
-                        </div>
-                        <div className="form-group">
-                            <Field name="country" type="text" placeholder="Enter Country"  className="form-control" />
-                            <ErrorMessage  name="country" />
-                        </div>
-                        <div className="form-group">
-                            <Field name="pincode" type="number" placeholder="Enter Pincode" className="form-control" />
-                            <ErrorMessage  name="pincode" />
-                        </div>
-                    </div>
-                    <div className="form-group w3-center">
-                        <button className="w3-button w3-purple" type="submit">Submit</button>
-                    </div>
-                </Form>
+                                    update && <button className="w3-button w3-purple" type="button" onClick={() => Update(initialValues, false, formikProps.values )}>Update</button>
+
+                                }
+
+                            </div>
+                        </Form>
+                    )}
+                }
+
+
             </Formik>
         </div>
     )
--- src/component/address/index.js
+++ src/component/address/index.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React,{useState} from 'react'
 import InputEntry from './InputEntry'
+import ViewAddress from './ViewAddress'
+import {http} from '../../axios'
 
 function Address() {
+    const [reload,setReload] = useState(1)
+
+    const Reload = (id)=>{
+        console.log(id)
+        setReload(id) 
+    }
+
+    const [update, setUpdate] = useState(false)
+    const [updateValues, setupdateValues] = useState({})
+    const [insert, setInsert] = useState(true)
+
+    const Update = (data,status,values) =>{
+        console.log(data,status)
+        if(status){            
+            setUpdate(status)
+            setupdateValues(data)
+            setInsert(false)
+        }
+        else{
+            editItem(data,status,values)            
+        }
+    }
+
+    const editItem=(data,status,values)=>{
+        http.put("address",values)
+        .then(res=>{
+            console.log(res.data)
+            setReload(res.data)  
+            setUpdate(status)
+            setupdateValues(data)
+            setInsert(true) 
+        })
+        .catch(err=>{
+            console.log(err)
+            alert("Error",err)
+        })
+    }
+
     return (
-        <div>
-            <InputEntry />
+        <div className="container">
+            <InputEntry 
+                Reload={Reload}                 
+                updateValues={updateValues} 
+                Update={Update}
+                insert={insert}
+                update={update}
+            />
+            <ViewAddress reload={reload} Update={Update} />
         </div>
     )
 }
